d7333b28765b19b6b6a9b3ed9af917b735597136
lucasTheProgrammer <134975545+lucasTheProgrammer@users.noreply.github.com>
2023-11-13 19:38:17 -0300

    Dfafqef

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@ const form = document.getElementById("novoitem")
 form.addEventListener("submit" , (evento) => {
   evento.preventDefault()
 
-  console.log(evento)
+ 
   console.log(evento.target.elements['nome'].value)
   console.log(evento.target.elements['quantidade'].value)
+  criaElemento(evento.target.elements['nome'].value, evento f)
 })
+
+function criaElemento(nome, quantidade) {
+
+
+}
